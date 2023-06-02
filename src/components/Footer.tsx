d2b380ca38640cd1b8e7445e7b132609f9450a6a
Axel Verglas <axelverglas@MacBook-Pro-de-Axel.local>
2023-06-02 09:39:02 +0200

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,12 +8,12 @@ export default function Footer(){
                 <div className='flex items-center justify-center my-12'>
                     <Image src="./img/logo.svg" alt="Kulteo" width={300} height={150} />
                 </div>
-                <div className='border-whitesmoke border-[0.75px]'></div>
+                <div className='border-whitesmoke border-[0.5px]'></div>
                 <div className='flex items-center justify-center my-8 text-whitesmoke'>
                     <Link href={'/'}>
                         Mentions légales
                     </Link>
-                    <span className='mx-1 '>-</span>
+                    <span className='mx-1'>-</span>
                     <p>Étudiants à DC Paris</p>
                 </div>
             </div>
